13de1d6f5d98af4901e408dcf5a6c7518c7e5b79
BBode11 <bode11@mail.nmc.edu>
2021-11-27 16:00:28 -0500

    Add sort, search, and filter functionality. Updated views.

--- Pages/Views/Index.cshtml.cs
+++ Pages/Views/Index.cshtml.cs
@@ -20,10 +20,58 @@ namespace Upbeat.Pages.Views
         }
 
         public IList<Songs> Songs { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string ArtistSort { get; set; }
+        public string TitleSort { get; set; }
+        public string DateSort { get; set; }
+        public string GenreSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Songs = await _context.Songs.ToListAsync();
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "TitleDesc" : "";
+            //ArtistSort = String.IsNullOrEmpty(sortOrder) ? "ArtistDesc" : "";
+            ArtistSort = sortOrder == "ArtistAsc" ? "ArtistDesc" : "ArtistAsc";
+            GenreSort = sortOrder == "GenreAsc" ? "GenreDesc" : "GenreAsc";
+            DateSort = sortOrder == "DateAsc" ? "DateDesc" : "DateAsc"; 
+
+            var songs = from s in _context.Songs
+                        select s;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                songs = songs.Where(s => s.Title.Contains(SearchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "TitleDesc":
+                    songs = songs.OrderByDescending(b => b.Title);
+                    break;
+                case "ArtistAsc":
+                    songs = songs.OrderBy(s => s.Artist);
+                    break;
+                case "ArtistDesc":
+                    songs = songs.OrderByDescending(s => s.Artist);
+                    break;
+                case "GenreAsc":
+                    songs = songs.OrderBy(s => s.Genre);
+                    break;
+                case "GenreDesc":
+                    songs = songs.OrderByDescending(s => s.Genre);
+                    break;
+                case "DateDesc":
+                    songs = songs.OrderByDescending(b => b.ReleaseDate);
+                    break;
+                case "DateAsc":
+                    songs = songs.OrderBy(b => b.ReleaseDate);
+                    break;
+                default:
+                    songs = songs.OrderBy(b => b.Title);
+                    break;
+            }
+
+            Songs = await songs.AsNoTracking().ToListAsync();
         }
 
 
